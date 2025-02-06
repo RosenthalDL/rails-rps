86c762fa31f1ccd81072893fd63d1deef21bd78f
Danielle Rosenthal <danieller530@gmail.com>
2025-02-06 23:19:26 +0000

    adding final product

---
+++ app/controllers/play_controller.rb
@@ -0,0 +1,38 @@
+class PlayController < ApplicationController
+    def rock
+      @comp_move = ["rock", "paper", "scissors"].sample
+      if @comp_move == "rock"
+        @outcome = "tied"
+      elsif @comp_move == "paper"
+        @outcome = "lost"
+      else
+        @outcome = "won"
+      end
+      render({ :template => "game_templates/play_rock"})
+    end
+
+    def paper
+      @comp_move = ["rock", "paper", "scissors"].sample
+      if @comp_move == "rock"
+        @outcome = "won"
+      elsif @comp_move == "paper"
+        @outcome = "tied"
+      else
+        @outcome = "lost"
+      end
+      render({ :template => "game_templates/play_paper"})
+    end
+
+    def scissors
+      @comp_move = ["rock", "paper", "scissors"].sample
+      if @comp_move == "rock"
+        @outcome = "lost"
+      elsif @comp_move == "paper"
+        @outcome = "won"
+      else
+        @outcome = "tied"
+      end
+      render({ :template => "game_templates/play_scissors"})
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get("/",{:controller => "homepage", :action => "home"} )
+  
+  get("/rock", {:controller => "play", :action => "rock"})
 
+  get("/paper", {:controller => "play", :action => "paper"})
+
+  get("/scissors", {:controller => "play", :action => "scissors"})
 end
